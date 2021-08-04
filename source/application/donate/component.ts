e4c9c88e8a2ae6c9a50cd9873fe5d5e830d6f885
Yassa Salib <yassa.salib@interfaceagency.com>
2021-08-04 11:16:24 +0200

    Update Checking styles

--- source/application/donate/component.ts
+++ source/application/donate/component.ts
@@ -1,8 +1,13 @@
 import { Component, ViewEncapsulation } from '@angular/core';
+import { faCheck, faCaretDown } from '@fortawesome/pro-solid-svg-icons';
+import type { IconProp } from '@fortawesome/fontawesome-svg-core';
 
 @Component({
 	selector: 'legal-donate',
 	templateUrl: './index.html',
 	encapsulation: ViewEncapsulation.None,
 })
-export class donateComponent {}
+export class donateComponent {
+	public faCheck: IconProp = faCheck;
+	public faCaretDown: IconProp = faCaretDown;
+}
