230f18e3c7364c08dbf253a7a454cfd8d7e4a061
Yassa Salib <yassa.salib@interfaceagency.com>
2021-08-07 12:56:56 +0200

    Update Video

--- source/application/video/module.ts
+++ source/application/video/module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
+import { VideoRoutingModule } from './routes';
 import { SharedModule } from '_application/shared/module';
 import { VideoListComponent } from './list/component';
 
 @NgModule({
 	declarations: [VideoListComponent],
-	imports: [SharedModule],
+	imports: [SharedModule, VideoRoutingModule],
 	providers: [],
 })
 export class VideoModule {}
